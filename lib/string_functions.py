8597baa47a44e5c25a47a4c07adbb1f403a0727f
Ronney Arias <ronneyarias@gmail.com>
2024-05-03 10:10:00 -0500

    complete lab

--- lib/string_functions.py
+++ lib/string_functions.py
@@ -1,5 +1,5 @@
 def return_string():
-    return ''
+    return 'im a string'
 
 def interpolate_string(s):
     return f'Hello, {s}!'
--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -2,6 +2,7 @@
 
 from not_none_functions import return_not_none
 
-# def test_return_not_none():
-#     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
-#     assert False
+def test_return_not_none():
+    '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
+    result = return_not_none()
+    assert result is not None
